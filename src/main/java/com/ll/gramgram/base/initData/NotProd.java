47978dc1665e41509ef924b6f797e805b41ee79c
minizi <64017307+leemimi@users.noreply.github.com>
2023-05-08 17:20:52 +0900

    3 week (#5)
    * 호감 취소/변경 로직 구현
    
    * read me 추가
    
    * readme수정
    
    * test전부다시추가

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -62,6 +62,7 @@ public class NotProd {
                 Ut.reflection.setFieldValue(likeablePersonToinstaUser4, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
                 LikeablePerson likeablePersonToinstaUser100 = likeablePersonService.like(memberUser3, "insta_user100", 2).getData();
                 Ut.reflection.setFieldValue(likeablePersonToinstaUser100, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
+                likeablePersonService.like(memberUser3, "insta_user2", 2).getData();
             }
         };
     }
